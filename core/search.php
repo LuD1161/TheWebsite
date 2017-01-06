068595d2ab4cb07c49b509f7fc3adbcda69e1ba4
LuD1161 <aseemshrey@gmail.com>
2017-01-06 23:03:42 +0530

    Minor Changes

--- core/search.php
+++ core/search.php
@@ -1,4 +1,13 @@
 //Search Bar
+include_once '../inc/dbconfig.inc.php';
 <?php
-
+  if(isset($_GET['u'])){
+    $username = mysql_real_escape_string($_GET['u']);
+    if(ctype_alnum($username)){
+      $result = $db_conn->query("SELECT user_name,joining_date,user_email from tbl_users    where user_name='$username'");
+      if(mysql_num_rows($result) > 0){
+        
+      }
+    }
+  }
  ?>
---
+++ core/user.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include_once 'dbconfig.inc.php';
+
+$stmt = $db_con->prepare("SELECT * FROM tbl_users WHERE user_id=:uid");
+$stmt->execute(array(":uid"=>$_SESSION['user_session']));
+$row=$stmt->fetch(PDO::FETCH_ASSOC);
+
+// $db_conn = mysqli_connect('localhost', 'iProject', '12345', 'webregistration') or die('Error connecting to database - registration ');
+
+
+if(isset($_GET["profile_id"])){
+  $user_id = trim($_GET["profile_id"]);
+  // $query = "SELECT * FROM tbl_users where user_id=". $user_id;
+  // $result = mysqli_query($db_conn, $query) or die('Error Conecting to Database - In fetching Data'. mysqli_error($db_conn));
+//  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+  if($row['user_id'] == $user_id){
+    echo $row['user_pic'];
+  }else {
+    echo "Not ".$user_id;
+  }
+}
+else {
+  echo "not".$_COOKIE["user_id"];
+}
+ ?>
