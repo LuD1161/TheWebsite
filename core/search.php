b3b32d16f99cd372f1676519f1a3d5871d433899
LuD1161 <aseemshrey@gmail.com>
2017-01-09 18:59:10 +0530

    Changed A Few Files

--- core/search.php
+++ core/search.php
@@ -3,15 +3,20 @@
 require_once '../inc/dbconfig.inc.php';
   if(isset($_POST['q'])){
     $username = $_POST['q'];
-    echo "Working";
     if(ctype_alnum($username)){
-      $result = $db_conn->query("SELECT user_name,joining_date,user_email from tbl_users    where user_name='$username'");
-      if(mysql_num_rows($result) > 0){
-        echo json_encode($result);
+      $result = $db_conn->query("SELECT user_name,joining_date,user_email from tbl_users where user_name='$username'");
+      if(count($result) > 0){
+        foreach ($result as $row) {
+          $data = array('Username' => $row['user_name'], 'Joined On' => $row['joining_date'], 'User Mail' => $row['user_email'] );
+          $rows[] = $data;
         }
-        else{
-          echo "<h1>No User as such</h1>";
+        if(count($result) > 0){
+          echo json_encode($rows);
         }
       }
+      else {
+        echo "<h1>No User as such</h1>";
+      }
     }
+  }
  ?>
