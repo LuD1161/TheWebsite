efbea68bb4e1fb2f6cbec00d02229664e0e14460
LuD1161 <aseemshrey@gmail.com>
2017-01-11 00:10:19 +0530

    Changed the layout of home page Added footer and two sidenav , maybe used later

--- login/home.php
+++ login/home.php
@@ -18,12 +18,7 @@ setcookie('user_id', $row['user_id'], time()+60, "/", "", 0);
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Member's Arena</title>
-<script src="../js/jquery.js"></script>
-<script src="../bootstrap/js/bootstrap.min.js"></script>
-<link href="home.css" rel="stylesheet" media="screen" />
-<script src="../js/memberArena.js"></script>
-<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
-<link href="../bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen" />
+<?php include('../inc/scripts.inc.php') ?>
 
 </head>
 <!-- The navbar is here -->
@@ -38,7 +33,33 @@ setcookie('user_id', $row['user_id'], time()+60, "/", "", 0);
     </p>
 </div>
 <div id="userBody">
-
 </div>
+<div class="container-fluid text-center" >
+  <div class="row-content">
+    <div class="col-sm-2 " >
+      <p><a href="#">maybe Profile</a></p>
+      <p><a href="#">Maybe Photo</a></p>
+      <p><a href="#">maybe About</a></p>
+    </div>
+    <div class="col-sm-8 text-left mainBody">
+      <h1 id="headingInside"></h1>
+
+
+    </div>
+    <div class="col-sm-2 ">
+      <div class="well">
+        <p>Chat Box Here</p>
+      </div>
+      <div class="well">
+        <p>Chat Box Here</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="container-fluid text-center" id="foot" >
+  <p ><span class='glyphicon glyphicon-copyright-mark'></span><strong> Aseem Shrey</strong> </p>
+</footer>
+
 </body>
 </html>
