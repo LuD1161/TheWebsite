f700a1aab62600ff120fa72c50c685ec3bebd4be
LuD1161 <aseemshrey@gmail.com>
2017-01-11 00:12:35 +0530

    Changing the structure of the project

---
+++ inc/scripts.inc.php
@@ -0,0 +1,8 @@
+<script src="../js/jquery.js"></script>
+<script src="../js/memberArena.js"></script>
+<script src="../js/functions.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+<link href="../css/home.css" rel="stylesheet" media="screen" />
+<link href="../css/searchResults.css" rel="stylesheet" media="screen" />
+<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+<link href="../bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen" />
