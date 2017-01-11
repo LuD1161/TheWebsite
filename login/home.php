4d5604893cdeeff280859b8bbb2aa82ed3fad036
LuD1161 <aseemshrey@gmail.com>
2017-01-11 23:20:28 +0530

    Design changes

--- login/home.php
+++ login/home.php
@@ -32,34 +32,7 @@ setcookie('user_id', $row['user_id'], time()+60, "/", "", 0);
       Welcome to the members page.
     </p>
 </div>
-<div id="userBody">
-</div>
-<div class="container-fluid text-center" >
-  <div class="row-content">
-    <div class="col-sm-2 " >
-      <p><a href="#">maybe Profile</a></p>
-      <p><a href="#">Maybe Photo</a></p>
-      <p><a href="#">maybe About</a></p>
-    </div>
-    <div class="col-sm-8 text-left mainBody">
-      <h1 id="headingInside"></h1>
-
-
-    </div>
-    <div class="col-sm-2 ">
-      <div class="well">
-        <p>Chat Box Here</p>
-      </div>
-      <div class="well">
-        <p>Chat Box Here</p>
-      </div>
-    </div>
-  </div>
-</div>
-
-<footer class="container-fluid text-center" id="foot" >
-  <p ><span class='glyphicon glyphicon-copyright-mark'></span><strong> Aseem Shrey</strong> </p>
-</footer>
-
+<?php include('../inc/body.inc.php')?>
 </body>
+
 </html>
