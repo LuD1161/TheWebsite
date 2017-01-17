545463b9d56458c7ab86d0f5f8f8c2c1e1d5c17e
LuD1161 <aseemshrey@gmail.com>
2017-01-18 00:05:49 +0530

    Return to the page from which it was called i.e. changed the header 'Location'.

--- login/upload.php
+++ login/upload.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-include_once 'dbconfig.php';
+include_once '../inc/dbconfig.inc.php';
 
-$stmt = $db_con->prepare("SELECT * FROM tbl_users WHERE user_id=:uid");
+$stmt = $db_conn->prepare("SELECT * FROM tbl_users WHERE user_id=:uid");
 $stmt->execute(array(":uid"=>$_SESSION['user_session']));
 $row=$stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -13,9 +13,10 @@ $target_file = $target_dir . "/" .$row['user_id'];
 $uploadOk = 1;
 $FileType = pathinfo($target_file,PATHINFO_EXTENSION);
 $target_file .=$FileType;
-echo $target_file."\n";
+$fileToUpload = $_FILES['fileToUpload']['name'];
+// echo $target_file."\n";
 
-$query = "UPDATE tbl_users set user_pic='". $target_file ."' where user_id='".$_COOKIE['user_id']."'";
+$query = "UPDATE tbl_users set user_pic='". $target_file ."' where user_id='".$_SESSION['user_session']."'";
 
 
 // Check if $uploadOk is set to 0 by an error
@@ -23,12 +24,12 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
         $affect = mysqli_query($db_conn, $query) or die('Error Conecting to Database - In fetching Data'. mysqli_error($db_conn));
         echo "affected rows were ".$affect;
 
-        header("Location: home.php");
+        header("Location:".$_SERVER['REQUEST_URI']);
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
